db23241b19527978b54938fbb0183ea388551b3d
Developer <mathonsiphumzile@gmail.com>
2018-05-28 09:15:49 +0200

    added activity

---
+++ app/src/main/java/com/example/developer/scannercode/StartsSetUp.java
@@ -0,0 +1,13 @@
+package com.example.developer.scannercode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartsSetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starts_set_up);
+    }
+}
