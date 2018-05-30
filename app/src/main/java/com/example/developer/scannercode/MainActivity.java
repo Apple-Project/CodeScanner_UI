934d0dfef7af89cf795bb088c521f108d1a2c8d3
Developer <mathonsiphumzile@gmail.com>
2018-05-30 15:08:15 +0200

    functional panic and scan

--- app/src/main/java/com/example/developer/scannercode/MainActivity.java
+++ app/src/main/java/com/example/developer/scannercode/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.developer.scannercode;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_SCANNER = 1;
@@ -15,12 +28,15 @@ public class MainActivity extends AppCompatActivity {
     public final static String CONTENT = "content";
     public final static String LOCATION = "location";
     public final static String TIMESTAMP = "time";
+    private static final String TAG = "Firestore" ;
 
     private TextView content;
     private TextView format;
     private TextView location;
     private TextView time;
-
+    // Access a Cloud Firestore instance from your Activity
+    public static final String TAG1 = "FIRESTORE";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +64,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void panicButton(View view){
+        // Create a panic event
+        Map<String, Object> event = new HashMap<>();
+        event.put("SiteID", "Mathonsi");
+        event.put("eventID", 8);
+        event.put("description", "Panic");
+        event.put("timeStamp", FieldValue.serverTimestamp());
+        event.put("location", "N-S");
+        Toast.makeText(this,"PANIC SENT", Toast.LENGTH_LONG).show();
+        addSite(db, "patrolData", event);
+    }
+
+    public void addSite(FirebaseFirestore db, String collectionName, Map object){
+
+        db.collection(collectionName).add(object).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID" + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+
+    }
+
+
     /*
     public void startSetup(View view){
         Intent intent = new Intent(MainActivity.this, StartsSetUp.class);
@@ -61,9 +107,15 @@ public class MainActivity extends AppCompatActivity {
         Bundle bundle = data.getExtras();
         //String content_ = bundle.getString(CONTENT);
         content.setText(bundle.getString(CONTENT));
+        // Create a new user with a first and last name
+
 
         //location.setText(bundle.getString(LOCATION));
         time.setText(bundle.getString(TIMESTAMP));
+        //Push to server
+
+
+
 
         /*
         content.setText(data.getStringExtra(CONTENT));
--- app/src/main/java/com/example/developer/scannercode/ScannerActivity.java
+++ app/src/main/java/com/example/developer/scannercode/ScannerActivity.java
@@ -3,6 +3,7 @@ package com.example.developer.scannercode;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,19 +12,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.Result;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
     private ZXingScannerView scannerView;
     private Calendar calendar;
+    public static final String TAG = "FIRESTORE";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,24 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
         scannerView = new ZXingScannerView(this);
         contentFrame.addView(scannerView);
     }
+
+
+    public void addSite(FirebaseFirestore db, String collectionName, Map object){
+
+        db.collection(collectionName).add(object).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID" + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+
+    }
+
 
 
     @Override
@@ -110,7 +141,15 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
                             }
                         });
 
-
+        // Create a scan event
+        Map<String, Object> event = new HashMap<>();
+        event.put("SiteID", "Mathonsi");
+        event.put("eventID", 0);
+        event.put("description", scan_Result);
+        event.put("timeStamp", FieldValue.serverTimestamp());
+        event.put("location", "N-S");
+        Toast.makeText(this,"POINT SCANNED", Toast.LENGTH_LONG).show();
+        addSite(db, "patrolData", event);
         builder.setMessage(scan_Result);
         AlertDialog alert = builder.create();
         alert.show();
